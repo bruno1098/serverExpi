5870a5dbb012f84c9baa240c18d13e0816591753
bruno1098 <rm98470@fiap.com.br>
2024-09-09 16:07:00 -0300

    deploy env

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const WebSocket = require('ws');
 
 // Criar o servidor HTTP
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Servidor WebSocket está rodando!');
+  res.writeHead(404);
+  res.end('Not Found');
 });
 
 // Use a porta fornecida pelo Heroku ou padrão para 8080 em desenvolvimento local
 const PORT = process.env.PORT || 8080;
 
-// Criar o servidor WebSocket usando o servidor HTTP
-const wss = new WebSocket.Server({ server });
+// Criar o servidor WebSocket com base no servidor HTTP
+const wss = new WebSocket.Server({ noServer: true });
+
+// Lidar com as conexões WebSocket
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request);
+  });
+});
 
 wss.on('connection', (ws) => {
   console.log('Novo cliente conectado');
@@ -19,7 +26,7 @@ wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log('Mensagem recebida:', message);
 
-    // Enviar mensagem para todos os clientes conectados
+    // Enviar a mensagem para todos os clientes conectados
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
@@ -32,7 +39,7 @@ wss.on('connection', (ws) => {
   });
 });
 
-// Escutar a porta correta
+// Iniciar o servidor na porta correta
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
